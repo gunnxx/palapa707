3df7179ef475e31546def531f8ea34d567f6ecaf
gunnxx <wahyu.guntara@gmail.com>
2018-09-19 15:13:33 +0700

    Remove build folder

--- build/krti18/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/Documents/auav/palapa707/src/krti18
-# Build directory: /home/ubuntu/Documents/auav/palapa707/build/krti18
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_krti18_roslaunch-check_launch "/home/ubuntu/Documents/auav/palapa707/build/catkin_generated/env_cached.sh" "/usr/bin/python" "/opt/ros/kinetic/share/catkin/cmake/test/run_tests.py" "/home/ubuntu/Documents/auav/palapa707/build/test_results/krti18/roslaunch-check_launch.xml" "--return-code" "/usr/bin/cmake -E make_directory /home/ubuntu/Documents/auav/palapa707/build/test_results/krti18" "/opt/ros/kinetic/share/roslaunch/cmake/../scripts/roslaunch-check -o '/home/ubuntu/Documents/auav/palapa707/build/test_results/krti18/roslaunch-check_launch.xml' '/home/ubuntu/Documents/auav/palapa707/src/krti18/launch' ")
